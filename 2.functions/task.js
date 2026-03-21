533a7be2b9436b6853b3f49617cc027963abb932
Ivan <xcaliburr24@gmail.com>
2026-03-21 14:19:55 +0300

    Task #1 finish

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,22 @@
 function getArrayParams(...arr) {
-
+let min = arr[0];
+let max = arr[0];
+let sum = 0;
+for(let i = 0; i < arr.length; i++) {
+  if(min > arr[i]) {
+    min = arr[i];
+  } 
+  if(max < arr[i]) { 
+    max = arr[i];
+    }
+    sum += arr[i];
+}
+let avg = Number((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
+getArrayParams(-10, 40, 20);
+getArrayParams(1, 2, 5, -10, 10);
+getArrayParams(10);
 
 function summElementsWorker(...arr) {
 
